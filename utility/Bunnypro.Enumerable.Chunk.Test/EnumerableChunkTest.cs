ba9206b32d0c366d749020b81ee0411854c496a2
albar <mubarrid.albar@outlook.com>
2019-07-05 14:04:02 +0700

    add utility

---
+++ utility/Bunnypro.Enumerable.Chunk.Test/EnumerableChunkTest.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using Xunit;
+
+namespace Bunnypro.Enumerable.Chunk.Test
+{
+    public class EnumerableChunkTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var items = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            const int size = 2;
+            
+            var expected = new[]
+            {
+                new[] {1, 2},
+                new[] {3, 4},
+                new[] {5, 6},
+                new[] {7, 8},
+                new[] {9}
+            };
+
+            var actual = items.Chunk(size).Select(c => c.ToArray()).ToArray();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ utility/Bunnypro.Enumerable.Chunk/EnumerableChunkExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Bunnypro.Enumerable.Chunk
+{
+    public static class EnumerableChunkExtension
+    {
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(
+            this IEnumerable<T> source,
+            int size)
+        {
+            using (var enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext()) yield return Take(enumerator, size);
+            }
+        }
+        
+        private static IEnumerable<T> Take<T>(IEnumerator<T> enumerator, int size)
+        {
+            var i = 0;
+            do
+            {
+                yield return enumerator.Current;
+                i++;
+            } while (i < size && enumerator.MoveNext());
+        }
+    }
+}
