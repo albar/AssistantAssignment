18acfcfdcd9e70dadd1e0aa15c585a438de156ef
albar <mubarrid.albar@outlook.com>
2019-07-05 14:54:28 +0700

    extract enumerable chunk sub operation to its own class

---
+++ utility/Bunnypro.Enumerable.Chunk/EnumerableChunk.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bunnypro.Enumerable.Chunk
+{
+    public class EnumerableChunk<T> : IEnumerable<T>
+    {
+        private readonly IEnumerator<T> _enumerator;
+        private readonly int _size;
+
+        public EnumerableChunk(IEnumerator<T> enumerator, int size)
+        {
+            _enumerator = enumerator;
+            _size = size;
+        }
+
+        private IEnumerable<T> AsEnumerable()
+        {
+            var i = 0;
+            do
+            {
+                yield return _enumerator.Current;
+                i++;
+            } while (i < _size && _enumerator.MoveNext());
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
--- utility/Bunnypro.Enumerable.Chunk/EnumerableChunkExtension.cs
+++ utility/Bunnypro.Enumerable.Chunk/EnumerableChunkExtension.cs
@@ -3,25 +3,16 @@
 namespace Bunnypro.Enumerable.Chunk
 {
     public static class EnumerableChunkExtension
-    {
-        public static IEnumerable<IEnumerable<T>> Chunk<T>(
-            this IEnumerable<T> source,
-            int size)
+    {   
+        public static IEnumerable<EnumerableChunk<T>> Chunk<T>(this IEnumerable<T> source, int size)
         {
             using (var enumerator = source.GetEnumerator())
             {
-                while (enumerator.MoveNext()) yield return Take(enumerator, size);
+                while (enumerator.MoveNext()) yield return new EnumerableChunk<T>(enumerator, size);
             }
         }
         
-        private static IEnumerable<T> Take<T>(IEnumerator<T> enumerator, int size)
         {
-            var i = 0;
-            do
-            {
-                yield return enumerator.Current;
-                i++;
-            } while (i < size && enumerator.MoveNext());
         }
     }
 }
