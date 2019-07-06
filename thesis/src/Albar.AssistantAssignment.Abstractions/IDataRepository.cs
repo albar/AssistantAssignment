042c95522fde6cc33866be230d010b14c9d4dbc5
albar <mubarrid.albar@outlook.com>
2019-07-06 21:26:23 +0700

    limit GeneSize to byte size

--- thesis/src/Albar.AssistantAssignment.Abstractions/IDataRepository.cs
+++ thesis/src/Albar.AssistantAssignment.Abstractions/IDataRepository.cs
@@ -5,9 +5,9 @@ namespace Albar.AssistantAssignment.Abstractions
 {
     public interface IDataRepository
     {
-        int GeneSize { get; }
-        ImmutableArray<ISchedule> Schedules { get; }
+        byte GeneSize { get; }
         ImmutableArray<ISubject> Subjects { get; }
+        ImmutableArray<ISchedule> Schedules { get; }
         ImmutableArray<IAssistant> Assistants { get; }
         ImmutableArray<IAssistantCombination> AssistantCombinations { get; }
     }
