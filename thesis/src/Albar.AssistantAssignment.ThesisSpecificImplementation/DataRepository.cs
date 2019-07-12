7414b053b7757bf3cc1750adc79a86c74388315a
albar <mubarrid.albar@outlook.com>
2019-07-12 19:09:28 +0700

    formatting

--- thesis/src/Albar.AssistantAssignment.ThesisSpecificImplementation/DataRepository.cs
+++ thesis/src/Albar.AssistantAssignment.ThesisSpecificImplementation/DataRepository.cs
@@ -39,13 +39,16 @@ namespace Albar.AssistantAssignment.ThesisSpecificImplementation
         private ImmutableArray<IAssistantCombination> CombineAssistants()
         {
             var combined = Subjects.SelectMany(subject =>
-                Assistants.Where(a => a.Subjects.Contains(subject.Id))
+                Assistants.Where(assistant => assistant.Subjects.Contains(subject.Id))
                     .Combine(subject.AssistantCountPerScheduleRequirement)
                     .Select(combination => new
                     {
                         Subject = subject.Id,
-                        Assistants = combination.Cast<Assistant>()
-                            .ToDictionary(a => a.Id, a => a.SubjectAssessments[subject.Id])
+                        AssistantsAssessments = combination.Cast<Assistant>()
+                            .ToDictionary(
+                                assistant => assistant.Id,
+                                assistant => assistant.SubjectAssessments[subject.Id]
+                            )
                     })
             ).ToArray();
             GeneSize = (byte) Math.Ceiling(Math.Log(combined.Length, 256));
@@ -54,12 +57,14 @@ namespace Albar.AssistantAssignment.ThesisSpecificImplementation
                 var assessmentCombination = Enum.GetValues(typeof(AssistantAssessment))
                     .Cast<AssistantAssessment>()
                     .ToDictionary(assessment => assessment, assessment =>
-                        combination.Assistants.Select(a => a.Value[assessment]).Max()
+                        combination.AssistantsAssessments
+                            .Select(assessments => assessments.Value[assessment])
+                            .Max()
                     );
                 return new AssistantCombination(
                     ByteConverter.GetByte(GeneSize, i),
                     combination.Subject,
-                    combination.Assistants.Select(a => a.Key),
+                    combination.AssistantsAssessments.Select(assistant => assistant.Key),
                     assessmentCombination
                 );
             }).Cast<IAssistantCombination>().ToImmutableArray();
