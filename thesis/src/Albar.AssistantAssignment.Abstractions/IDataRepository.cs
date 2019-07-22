4048c30cd820257d48a86eca7ad08381cba02698
albar <mubarrid.albar@outlook.com>
2019-07-22 13:53:01 +0700

    remove unnecessary comment

--- thesis/src/Albar.AssistantAssignment.Abstractions/IDataRepository.cs
+++ thesis/src/Albar.AssistantAssignment.Abstractions/IDataRepository.cs
@@ -8,7 +8,6 @@ namespace Albar.AssistantAssignment.Abstractions
     public interface IDataRepository<T> where T : Enum
     {
         byte GeneByteSize { get; }
-//        IReadOnlyDictionary<T, double> ObjectiveCoefficients { get; }
         ImmutableArray<ISubject> Subjects { get; }
         ImmutableArray<ISchedule> Schedules { get; }
         ImmutableArray<IAssistant> Assistants { get; }
